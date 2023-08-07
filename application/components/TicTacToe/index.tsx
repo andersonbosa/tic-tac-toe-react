c83e771853da8f455aed8aa81f9feb590101c4fa
andersonbosa <andersonbosa0@gmail.com>
2023-08-06 21:52:57 -0300

    Checkpoint

--- application/components/TicTacToe/index.tsx
+++ application/components/TicTacToe/index.tsx
@@ -4,9 +4,10 @@ import { Dispatch, SetStateAction, useState } from 'react'
 
 interface SquareProps {
   value: number
+  onSquareClick: Dispatch<SetStateAction<any>>
 }
 
-const Square: React.FC<SquareProps> = ({ value }) => {
+const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
   // const [value, setValue] = useState(null)
   // function onSquareClick () {
   // setValue('X')
@@ -14,7 +15,7 @@ const Square: React.FC<SquareProps> = ({ value }) => {
 
   return (
     < >
-      <button className='square' >{value}</button>
+      <button className='square' onClick={onSquareClick}>{value}</button>
     </>
   )
 }
@@ -22,25 +23,35 @@ const Square: React.FC<SquareProps> = ({ value }) => {
 
 const Board = () => {
   const ARRAY_SIZE = 9
+  const [squares, setSquares] = useState(Array(ARRAY_SIZE).fill(null))
 
-  const [squares, setSquares] = useState(
-    Array(ARRAY_SIZE).fill(null)
-  )
+  const [xIsNext, setXisNext] = useState(false)
+
+  function handlePlayerClick (i: number) {
+    const nextSquares = squares.slice()
+
+    nextSquares[i] = Boolean(xIsNext) ? "X" : "O"
+
+    setSquares(nextSquares)
+    setXisNext(!xIsNext)
+
+    console.log(nextSquares)
+  }
 
   return (
     <>
       <div className='board-row'></div>
-      <Square value={squares[0]} />
-      <Square value={squares[1]} />
-      <Square value={squares[2]} />
+      <Square value={squares[0]} onSquareClick={() => handlePlayerClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handlePlayerClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handlePlayerClick(2)} />
       <div className='board-row'></div>
-      <Square value={squares[3]} />
-      <Square value={squares[4]} />
-      <Square value={squares[5]} />
+      <Square value={squares[3]} onSquareClick={() => handlePlayerClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handlePlayerClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handlePlayerClick(5)} />
       <div className='board-row'></div>
-      <Square value={squares[6]} />
-      <Square value={squares[7]} />
-      <Square value={squares[8]} />
+      <Square value={squares[6]} onSquareClick={() => handlePlayerClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handlePlayerClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handlePlayerClick(8)} />
     </>
   )
 }
