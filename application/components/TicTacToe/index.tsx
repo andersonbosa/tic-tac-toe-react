a3ecf75ef7d60c847648cf7d5df63ef19b62e6ea
andersonbosa <andersonbosa0@gmail.com>
2023-08-06 22:21:30 -0300

    Checkpoint

--- application/components/TicTacToe/index.tsx
+++ application/components/TicTacToe/index.tsx
@@ -20,44 +20,117 @@ const Square: React.FC<SquareProps> = ({ value, onSquareClick }) => {
   )
 }
 
+function obterLinhasMatrizQuadrada (matriz) {
+  if (!Array.isArray(matriz) || matriz.length === 0 || matriz.some(row => !Array.isArray(row) || row.length !== matriz.length)) {
+    throw new Error('A entrada deve ser uma matriz de ordem quadrada.')
+  }
 
-const Board = () => {
-  const ARRAY_SIZE = 9
-  const [squares, setSquares] = useState(Array(ARRAY_SIZE).fill(null))
+  const n = matriz.length
+  const linhas = []
 
-  const [xIsNext, setXisNext] = useState(false)
+  // Adiciona as linhas da matriz
+  for (let i = 0; i < n; i++) {
+    linhas.push(matriz[i].slice())
+  }
 
-  function handlePlayerClick (i: number) {
+  // Adiciona as colunas da matriz
+  for (let i = 0; i < n; i++) {
+    const coluna = []
+    for (let j = 0; j < n; j++) {
+      coluna.push(matriz[j][i])
+    }
+    linhas.push(coluna)
+  }
+
+  // Adiciona a diagonal principal
+  const diagonalPrincipal = []
+  for (let i = 0; i < n; i++) {
+    diagonalPrincipal.push(matriz[i][i])
+  }
+  linhas.push(diagonalPrincipal)
+
+  // Adiciona a diagonal secundária
+  const diagonalSecundaria = []
+  for (let i = 0; i < n; i++) {
+    diagonalSecundaria.push(matriz[i][n - 1 - i])
+  }
+  linhas.push(diagonalSecundaria)
+
+  return linhas
+}
+
+
+function Board () {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleClick (i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     const nextSquares = squares.slice()
-
-    nextSquares[i] = Boolean(xIsNext) ? "X" : "O"
-
+    if (xIsNext) {
+      nextSquares[i] = 'X'
+    } else {
+      nextSquares[i] = 'O'
+    }
     setSquares(nextSquares)
-    setXisNext(!xIsNext)
+    setXIsNext(!xIsNext)
+  }
 
-    console.log(nextSquares)
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
   }
 
   return (
     <>
-      <div className='board-row'></div>
-      <Square value={squares[0]} onSquareClick={() => handlePlayerClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handlePlayerClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handlePlayerClick(2)} />
-      <div className='board-row'></div>
-      <Square value={squares[3]} onSquareClick={() => handlePlayerClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handlePlayerClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handlePlayerClick(5)} />
-      <div className='board-row'></div>
-      <Square value={squares[6]} onSquareClick={() => handlePlayerClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handlePlayerClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handlePlayerClick(8)} />
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
   )
 }
 
-const TicTacToe = () => {
+function calculateWinner (squares: Array<Array<number>>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
 
+
+
+const TicTacToe = () => {
   return (
     <>
       <Board />
